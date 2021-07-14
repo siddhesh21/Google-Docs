9f53bfd074d36cee2489cae120ab48111423983e
Siddhesh Kankekar <kankekas@tcd.ie>
2021-07-14 13:36:40 +0100

    implemented Firebase

--- google-docs/firebase.js
+++ google-docs/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBH0LsljD67jGV0abDOeZ368tsoh9X0GrE",
   authDomain: "docs-dark.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "311932049190",
   appId: "1:311932049190:web:5441c84002bf7ceed153e7",
 };
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export { db };
